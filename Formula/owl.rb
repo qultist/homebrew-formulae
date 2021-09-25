0c0707f92da9c0b5ee28915b1d238ef6a139592b
Dominic Schmidt <dev@abstraqt.de>
2021-09-25 18:55:17 +0200

    Update owl.rb

--- Formula/owl.rb
+++ Formula/owl.rb
@@ -1,7 +1,7 @@
 class Owl < Formula
   desc "Owl collects the licenses of the Swift packages you use"
   homepage "https://github.com/qultist/owl"
-  url "https://github.com/qultist/owl.git", tag: "0.1.0", revision: "1140e288a393fa9f61f51d6d3174462b636fc224"
+  url "https://github.com/qultist/owl.git", tag: "0.1.1", revision: "ac4a397247d34e08888d0478847d83b044517c6d"
   license "MIT"
 
   depends_on xcode: ["12.0", :build]
